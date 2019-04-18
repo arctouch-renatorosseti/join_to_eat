c096c1d63edd4e8134196d356402d7bcb55b6500
Renato Rosseti <renatorosseti@Renatos-MacBook-Pro.local>
2019-04-18 13:28:20 -0300

    Refactor strings resources.

--- lib/app/resources/strings.dart
+++ lib/app/resources/strings.dart
@@ -1,28 +1,10 @@
 //TODO: implement localization
 class Strings {
-  static const String appName = 'Scholar';
+  static const String appName = 'Join to eat';
   static const String email = 'Email';
-  static const String password = 'Senha';
   static const String login = 'Entrar';
-  static const String createAccount = 'Criar conta';
-  static const String resetPassword = 'Resetar senha';
   static const String createAnAccount = 'Criar uma conta';
   static const String unknownError = 'Erro inesperado, tente novamente mais tarde.';
-  static const String errorUserDisabled = 'Usuário desativado.';
-  static const String errorTooManyRequests = 'Muitas requisições, tente novamente em instantes.';
-  static const String errorOperationNotAllowed = 'Operação não permitida.';
-  static const String errorInvalidEmail = 'Email inválido, verifique o email digitado.';
-  static const String errorWrongPassword = 'Senha inválida ou inexistente.';
-  static const String errorUserNotFound = 'Usuário não encontrado.';
-  static const String haveAnAccount = 'Possui uma conta? Entrar';
-  static const String backToLogin = 'Voltar para tela de login';
-  static const String forgotPassword = 'Esqueceu sua senha?';
-  static const String emailSent = 'Email enviado';
-  static const String emailSentResetPassword = 'Um email foi enviado com instruções para a redefinição da sua senha';
+  static const String errorUserNotFound = 'Email não encontrado.';
   static const String ok = 'ok';
-  static const String home = 'Home';
-  static const String agenda = 'Agenda';
-  static const String profile = 'Perfil';
-  static const String emailCantBeEmpty = 'Email não pode ficar em branco';
-  static const String passwordCantBeEmpty = 'Senha não pode ficar em branco';
 }
