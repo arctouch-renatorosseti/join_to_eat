c7afe3ba0c54004f24b97fb789178b38714d96e9
Rafael Silva e Souza <rafael.souza@arctouch.com>
2019-04-23 18:35:23 -0300

    Changes to navigation and views.

--- lib/app/main_app.dart
+++ lib/app/main_app.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:join_to_eat/app/presentation/main_view.dart';
 import 'package:join_to_eat/app/presentation/map_view.dart';
 import 'package:join_to_eat/app/presentation/sign_in/auth_widget.dart';
 import 'package:join_to_eat/app/utils/routes.dart';
+
+import 'presentation/create_event_view.dart';
+import 'presentation/list_events_view.dart';
 
 class MainApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Join To Eat',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
       routes: {
         Routes.root: (c) => MyHomePage(title: 'Flutter Demo Home Page'),
         Routes.main: (c) => MapView(),
+        Routes.createEvent: (c) => CreateEventView(),
+        Routes.listEvents: (c) => ListEventsView(),
       },
     );
   }
@@ -31,11 +35,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-
       child: SignInWidget(),
     );
   }
--- lib/app/utils/routes.dart
+++ lib/app/utils/routes.dart
@@ -2,4 +2,6 @@ class Routes {
   static const login = "/login";
   static const main = "/main";
   static const root = "/";
+  static const createEvent = "/createEvent";
+  static const listEvents = "/listEvents";
 }
