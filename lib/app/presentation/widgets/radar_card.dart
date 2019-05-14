16840372ad28b14d1a61690fb4b447a3f959f432
Renato Rosseti <renatorosseti@Renatos-MacBook-Pro.local>
2019-05-14 16:40:31 -0300

    Add details screen.

--- lib/app/presentation/widgets/radar_card.dart
+++ lib/app/presentation/widgets/radar_card.dart
@@ -7,6 +7,7 @@ import 'package:join_to_eat/app/bloc/meeting/radar_card_bloc.dart';
 import 'package:join_to_eat/app/model/meeting.dart';
 import 'package:join_to_eat/app/resources/strings.dart';
 import 'package:join_to_eat/app/utils/ScalerHelper.dart';
+import 'package:join_to_eat/app/utils/routes.dart';
 import 'package:sprintf/sprintf.dart';
 
 class RadarCard extends StatefulWidget {
@@ -83,50 +84,59 @@ class _RadarCardState extends State<RadarCard> with SingleTickerProviderStateMix
                   margin: outCardMargin,
                   child: Padding(
                       padding: inCardMargin,
-                      child: Row(
-                        children: <Widget>[
-                          Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                            Text(
-                              state?.title ?? Strings.radarLoadingCard,
-                              style: titleStyle,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Opacity(
-                                    opacity: 0.5,
+                      child: InkWell(
+                          onTap: () {
+                            _onCardTapped();
+                          },
+                          child: Row(
+                            children: <Widget>[
+                              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                                Text(
+                                  state?.title ?? Strings.radarLoadingCard,
+                                  style: titleStyle,
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Opacity(
+                                        opacity: 0.5,
+                                        child: Text(
+                                          Strings.radarCreator,
+                                          style: infoStyle,
+                                        )),
+                                    Text(
+                                      state?.creator ?? Strings.radarLoadingCard,
+                                      style: infoStyle,
+                                    )
+                                  ],
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.only(top: ScalerHelper.getScaledValue(7.0)),
                                     child: Text(
-                                      Strings.radarCreator,
+                                      sprintf(Strings.radarPartySize, [state?.partySize]),
                                       style: infoStyle,
                                     )),
+                              ]),
+                              Expanded(
+                                  child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.only(bottom: ScalerHelper.getScaledValue(11.0)),
+                                    child: Text(
+                                      whenText,
+                                      style: dateStyle,
+                                    )),
                                 Text(
-                                  state?.creator ?? Strings.radarLoadingCard,
-                                  style: infoStyle,
-                                )
-                              ],
-                            ),
-                            Padding(
-                                padding: EdgeInsets.only(top: ScalerHelper.getScaledValue(7.0)),
-                                child: Text(
-                                  sprintf(Strings.radarPartySize, [state?.partySize]),
-                                  style: infoStyle,
-                                )),
-                          ]),
-                          Expanded(
-                              child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: <Widget>[
-                            Padding(
-                                padding: EdgeInsets.only(bottom: ScalerHelper.getScaledValue(11.0)),
-                                child: Text(
-                                  whenText,
-                                  style: dateStyle,
-                                )),
-                            Text(
-                              //TODO: Calculate distance and then use this: state.distance == 0 ? "" : sprintf(Strings.radarDistance, [state.distance]),
-                              happeningNow,
-                              style: distanceStyle,
-                            ),
-                          ])),
-                        ],
-                      ))));
+                                  //TODO: Calculate distance and then use this: state.distance == 0 ? "" : sprintf(Strings.radarDistance, [state.distance]),
+                                  happeningNow,
+                                  style: distanceStyle,
+                                ),
+                              ])),
+                            ],
+                          )))));
         });
+  }
+
+  _onCardTapped() {
+    print('Card tapped');
+    Navigator.pushNamed(context, Routes.meetingDetails);
   }
 }
